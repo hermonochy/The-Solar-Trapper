8ef9428930c1a14a5d64b239605ebcac199dce6a
hermonochy <hermonochy@users.noreply.github.com>
2025-03-06 21:28:14 +0000

    add arguments

--- main.py
+++ main.py
@@ -3,11 +3,34 @@ BASELINE ALGORITHM:
 
 Curtain is closing if not sunny (light > 15) for 30 mins, temperature dropped by 0.5 K in 30 min and it is the afternoon, or if it is close to sunset.  
 
+CLI PARAMETER:
+- Earliest close time
+- Latest close time
+- Temperature trigger difference
+- Temperature trigger time
+- Light level sunny
+- Light level trigger time
+- MAYBE: Data source (file or default read from sensor)
+- 
 """
 
 import queue
+import argparse
 import time
 from datetime import datetime
+
+parser = argparse.ArgumentParser(
+	prog='Curtain Closer',
+	description='Main program for Curtain Closer.',
+	)
+parser.add_argument('-b', '--beginning', nargs='?', const="", help="Earliest closing time", default=12)
+parser.add_argument('-e', '--end', nargs='?', const="", help="Latest closing time", default=17)
+parser.add_argument('-t', '--tempTrigger', nargs='?', const="", help=" Temperature trigger difference in K", default=0.5)
+parser.add_argument('-l', '--lightTrigger', nargs='?', const="", help="Max value for light level to be considered sunny", default=15)
+parser.add_argument('--triggerTime', nargs='?', const="", help="History in seconds for trigger", default=1800)
+
+args = parser.parse_args()
+
 
 q = queue.Queue()
 
@@ -17,7 +40,7 @@ with open("TempLog/20250223.csv") as file, open("closingTimes.txt", 'w') as outF
 
 	secondsWithoutSun = 0
 
-	for i,line in enumerate(file):	
+	for i, line in enumerate(file):	
 		
 		values = line[0:-1].split(",")
 
@@ -31,10 +54,10 @@ with open("TempLog/20250223.csv") as file, open("closingTimes.txt", 'w') as outF
 			print("ValueError")
 			continue
 
-		if q.qsize() <= 1800:
+		if q.qsize() <= args.triggerTime:
 			q.put(temp)
 
-		itIsSunnyNow = light < 15
+		itIsSunnyNow = light < args.lightTrigger
 
 		if not itIsSunnyNow:
 			secondsWithoutSun += 1
@@ -49,20 +72,20 @@ with open("TempLog/20250223.csv") as file, open("closingTimes.txt", 'w') as outF
 		if curtainClosed and timeLogged.hour == 8:
 			curtainClosed = False
 
-		if q.qsize()>1790:
-			temp1800secondsAgo = q.get()
+		if q.qsize()>args.triggerTime-1:
+			tempNsecondsAgo = q.get()
 			currentTemperature = temp
 
-			temperatureDroppedByHalfKelvin = currentTemperature + 0.5 < temp1800secondsAgo
-			print("temperatureDroppedByHalfKelvin ", temperatureDroppedByHalfKelvin, " currentTemperature ", currentTemperature, " temp1800secondsAgo ", temp1800secondsAgo)
+			temperatureDroppedByHalfKelvin = currentTemperature + args.tempTrigger < tempNsecondsAgo
+			print("temperatureDroppedByHalfKelvin ", temperatureDroppedByHalfKelvin, " currentTemperature ", currentTemperature, " tempNsecondsAgo ", tempNsecondsAgo)
 
 
-			if not curtainClosed and timeLogged.hour >= 12:
+			if not curtainClosed and timeLogged.hour >= args.beginning:
 					
 					
-					itWasntSunnyFor1800seconds = secondsWithoutSun >= 1800
+					itWasntSunnyForNseconds = secondsWithoutSun >= args.triggerTime
 
-					if itWasntSunnyFor1800seconds and temperatureDroppedByHalfKelvin:
+					if itWasntSunnyForNseconds and temperatureDroppedByHalfKelvin:
 						print("Closing now!")
 						time.sleep(5)
 						curtainClosed = True
